afb2702b4cc5d02e4b7329275db9cb3534fe7546
Feyza Keles <feyza05@live.de>
2021-08-30 12:26:07 +0200

    Letzter Stand 30. Aug

---
+++ interface/NavigationDrawer-master/NavigationDrawerPopUpMenu2/UserControlCreate.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NavigationDrawerPopUpMenu2
+{
+    public partial class UserControlCreate : UserControl
+    {
+        public UserControlCreate()
+        {
+            InitializeComponent();
+            var videos = GetVideos();
+            if (videos.Count > 0)
+                ListViewProducts.ItemsSource = videos;
+        }
+        private List<Videos> GetVideos()
+        {
+            return new List<Videos>()
+      {
+        new Videos("/Assets/1.jpg"),
+        new Videos("/Assets/2.jpg"),
+        new Videos("/Assets/3.jpg"),
+        new Videos("/Assets/4.jpg"),
+        new Videos("/Assets/1.jpg"),
+        new Videos("/Assets/2.jpg"),
+        new Videos("/Assets/3.jpg"),
+        new Videos("/Assets/4.jpg"),
+        new Videos("/Assets/1.jpg"),
+        new Videos("/Assets/2.jpg"),
+        new Videos("/Assets/3.jpg"),
+        new Videos("/Assets/4.jpg"),
+        new Videos("/Assets/2.jpg"),
+        new Videos("/Assets/3.jpg"),
+        new Videos("/Assets/4.jpg"),
+        new Videos("/Assets/1.jpg"),
+        new Videos("/Assets/2.jpg"),
+        new Videos("/Assets/3.jpg"),
+        new Videos("/Assets/4.jpg"),
+         new Videos("/Assets/2.jpg"),
+        new Videos("/Assets/3.jpg"),
+        new Videos("/Assets/4.jpg"),
+        new Videos("/Assets/1.jpg"),
+        new Videos("/Assets/2.jpg"),
+        new Videos("/Assets/3.jpg"),
+        new Videos("/Assets/4.jpg")
+
+      };
+        }
+    }
+}
---
+++ interface/NavigationDrawer-master/NavigationDrawerPopUpMenu2/Videos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationDrawerPopUpMenu2
+{
+    class Videos
+    {
+       // public string Name { get; set; }
+       // public double Value { get; set; }
+        public string Image { get; set; }
+
+        public Videos(string image)
+        {
+            Image = image;
+        }
+    }
+}
